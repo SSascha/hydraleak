58c77cfb2094b3cb846ae26291a4fd07c221cd48
SSascha <seefelder.sascha@gmail.com>
2016-04-26 17:39:54 +0200

    Tue, 26 Apr 2016 17:39:54 +0200

---
+++ 02_absdiff_test_same_pic/src/test.cpp
@@ -0,0 +1,23 @@
+
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+	Mat src = imread("/home/sascha/Bilder/test_02_a.jpg", 1); //1:color, 0:gray 
+	Mat dst = imread("/home/sascha/Bilder/test_02_a.jpg", 1); //1:color, 0:gray
+	Mat diff; 
+	absdiff(src, dst, diff);   
+	threshold(diff, diff, 10, 255, CV_THRESH_BINARY); // grayscale needed
+
+	while(1) {
+		imshow("original", src);   
+		imshow("new", dst);   
+		imshow("diff", diff);
+		waitKey(100);
+	}
+	return 0;
+}
